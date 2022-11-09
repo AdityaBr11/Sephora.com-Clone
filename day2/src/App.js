e32c540b4aae3645450ebed9108dfa879dfb8ad2
AdityaBr11 <adityaanandpurnea1@gmail.com>
2022-11-09 23:41:48 +0530

    day02

---
+++ day2/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ButtomPart from './Components/ButtomPart';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ButtomPart/> */}
+      <Navbar/>
+      <ButtomPart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day2/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FaFacebookF} from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <div>
+      {/* logo part */}
+      {/* <div className='h-28 bg-red-300  flex py-8 '>
+      
+       <div className="bg-green-200 flex w-5/6 m-auto gap-8">
+       <img src="https://i.postimg.cc/ZY9NdkKx/brandlogo.png" alt="SEPHORA" />
+        <input className='h-1/2 py-1 w-2/6' type="text" placeholder='Q Search' />
+        <h3><FaFacebookF/> </h3>
+       </div>
+
+      </div> */}
+    </div>
+  )
+}
+
+export default Navbar
